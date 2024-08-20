c315b3658fb252f34b5de42ad88d2d9eaae8b6e4
nexryai <nexryai@sda1.net>
2024-08-20 04:33:54 +0000

    Fix: アカウント削除周りの多数の不具合を修正 アカウント削除時にフォロリク周りの管理などが正常に行われない問題を修正 ref: https://github.com/atsu1125/misskey-v12/blob/42f1f0c14ea55ef66c1050827bd4eb7849640d64/packages/backend/src/queue/processors/db/delete-account.ts

--- packages/backend/src/queue/processors/db/delete-account.ts
+++ packages/backend/src/queue/processors/db/delete-account.ts
@@ -1,12 +1,14 @@
 import Bull from "bull";
-import { MoreThan } from "typeorm";
 import { AccessTokens, DriveFiles, Notes, UserProfiles, Users, UserNotePinings, MessagingMessages, Followings, Mutings, Blockings, Notifications, FollowRequests, Antennas, NoteReactions, Clips } from "@/models/index.js";
 import { DbUserDeleteJobData } from "@/queue/types.js";
 import { Note } from "@/models/entities/note.js";
 import { DriveFile } from "@/models/entities/drive-file.js";
 import { deleteFileSync } from "@/services/drive/delete-file.js";
-import { sendEmail } from "@/services/send-email.js";
 import { emailDeliver } from "@/queue/index.js";
+import deleteFollowing from "@/services/following/delete.js";
+import cancelFollowRequest from "@/services/following/requests/cancel.js";
+import { rejectFollowRequest } from "@/services/following/reject.js";
+import { publishUserEvent, publishInternalEvent } from "@/services/stream.js";
 import { queueLogger } from "../../logger.js";
 
 const logger = queueLogger.createSubLogger("delete-account");
@@ -19,9 +21,88 @@ export async function deleteAccount(job: Bull.Job<DbUserDeleteJobData>): Promise
         return;
     }
 
-    { // Delete notes
-        let cursor: Note["id"] | null = null;
+    { // Reject Follows
+        // When deleting a remote account, the account obviously doesn't
+        // actually become deleted on its origin server, i.e. unlike a
+        // locally deleted account it continues to have access to its home
+        // feed and other content. To prevent it from being able to continue
+        // to access toots it would receive because it follows local accounts,
+        // we have to force it to unfollow them.
 
+        if (!Users.isLocalUser(job.data.user)) {
+            const follower = user;
+
+            const followings = await Followings.findBy({
+                followerId: follower.id,
+            });
+
+            for (const following of followings) {
+                const followee = await Users.findOneBy({
+                    id: following.followeeId,
+                });
+
+                if (followee != null) {
+                    await deleteFollowing(follower, followee, true);
+                }
+            }
+
+            const requests = await FollowRequests.findBy({
+                followerId: follower.id,
+            });
+
+            for (const request of requests) {
+                const followee = await Users.findOneBy({
+                    id: request.followeeId,
+                });
+
+                if (followee != null) {
+                    await rejectFollowRequest(followee, follower);
+                }
+            }
+        }
+    }
+
+    { // Undo Follows
+        // When deleting a remote account, the account obviously doesn't
+    // actually become deleted on its origin server, but following relationships
+    // are severed on our end. Therefore, make the remote server aware that the
+    // follow relationships are severed to avoid confusion and potential issues
+    // if the remote account gets un-suspended.
+
+        if (!Users.isLocalUser(job.data.user)) {
+            const followee = user;
+
+            const followers = await Followings.findBy({
+                followeeId: followee.id,
+            });
+
+            for (const following of followers) {
+                const follower = await Users.findOneBy({
+                    id: following.followerId,
+                });
+
+                if (follower != null) {
+                    await deleteFollowing(follower, followee, true);
+                }
+            }
+
+            const requests = await FollowRequests.findBy({
+                followeeId: followee.id,
+            });
+
+            for (const request of requests) {
+                const follower = await Users.findOneBy({
+                    id: request.followerId,
+                });
+
+                if (follower != null) {
+                    await cancelFollowRequest(followee, follower);
+                }
+            }
+        }
+    }
+
+    { // Delete notes
         const notesCount = await Notes.createQueryBuilder("note")
     .where("note.userId = :userId", { userId: job.data.user.id })
     .getCount();
@@ -30,24 +111,15 @@ export async function deleteAccount(job: Bull.Job<DbUserDeleteJobData>): Promise
             const notes = await Notes.find({
                 where: {
                     userId: user.id,
-                    ...(cursor ? { id: MoreThan(cursor) } : {}),
                 },
-                take: 100,
-                order: {
-                    id: 1,
-                },
+                take: 50,
             }) as Note[];
 
             if (notes.length === 0) {
                 break;
             }
 
-            cursor = notes[notes.length - 1].id;
-
-            for (const note of notes) {
-                await Notes.delete(note.id);
-                await new Promise(resolve => setTimeout(resolve, 500)); // 0.5秒待機
-            }
+            await Notes.delete(notes.map(note => note.id));
 
             const currentNotesCount = await Notes.createQueryBuilder("note")
 			.where("note.userId = :userId", { userId: job.data.user.id })
@@ -62,25 +134,17 @@ export async function deleteAccount(job: Bull.Job<DbUserDeleteJobData>): Promise
     }
 
     { // Delete files
-        let cursor: DriveFile["id"] | null = null;
-
         while (true) {
             const files = await DriveFiles.find({
                 where: {
                     userId: user.id,
-                    ...(cursor ? { id: MoreThan(cursor) } : {}),
                 },
                 take: 10,
-                order: {
-                    id: 1,
-                },
             }) as DriveFile[];
 
             if (files.length === 0) {
                 break;
             }
-
-            cursor = files[files.length - 1].id;
 
             for (const file of files) {
                 await deleteFileSync(file);
@@ -102,6 +166,9 @@ export async function deleteAccount(job: Bull.Job<DbUserDeleteJobData>): Promise
     // soft指定されている場合は物理削除しない
     if (job.data.soft) {
         // nop
+        await MessagingMessages.delete({
+            userId: job.data.user.id,
+        });
     } else {
         if (Users.isLocalUser(job.data.user)) {
             await UserProfiles.update(job.data.user.id, {
@@ -178,6 +245,8 @@ export async function deleteAccount(job: Bull.Job<DbUserDeleteJobData>): Promise
             await Clips.delete({
                 userId: job.data.user.id,
             });
+            publishInternalEvent("userChangeSuspendedState", { id: job.data.user.id, isSuspended: true });
+            publishUserEvent(job.data.user.id, "terminate", {});
         } else {
             await Users.delete(job.data.user.id);
         }
