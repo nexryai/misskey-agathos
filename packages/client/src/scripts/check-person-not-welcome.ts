5d2f566f5f2904c3fed8267a5c0546c7d3b086c2
nexryai <nexryai@sda1.net>
2024-08-20 00:29:28 +0000

    Remove client

--- packages/client/src/scripts/check-person-not-welcome.ts
+++
@@ -1,9 +0,0 @@
-export function checkPersonNotWelcome(checkUser: string, mutedUsers: Array<string | string[]>) {
-    return mutedUsers.some(item => {
-        if (Array.isArray(item)) {
-            return item.includes(checkUser);
-        } else {
-            return item === checkUser;
-        }
-    });
-}
--- packages/client/src/scripts/check-reaction-mute.ts
+++
@@ -1,9 +0,0 @@
-export function checkReactionMute(reaction: string, mutedReactions: Array<string | string[]>) {
-    return mutedReactions.some(item => {
-        if (Array.isArray(item)) {
-            return item.includes(reaction);
-        } else {
-            return item === reaction;
-        }
-    });
-}
