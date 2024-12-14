256565a2bd96fd8d8bff66eb49c8e2089fa1761d
nexryai <nexryai@sda1.net>
2024-12-14 20:05:06 +0900

    remove an unused import

--- packages/backend/src/queue/processors/db/export-custom-emojis.ts
+++ packages/backend/src/queue/processors/db/export-custom-emojis.ts
@@ -1,7 +1,6 @@
 import * as fs from "node:fs";
 import Bull from "bull";
 
-import { ulid } from "ulid";
 import mime from "mime-types";
 import archiver from "archiver";
 import { format as dateFormat } from "date-fns";
